ed5a0bfbf706799cf49340789522936591b029f2
nsuperman <87185822+nsuperman@users.noreply.github.com>
2021-07-09 20:45:16 +0300

    Update nonlin.m

--- nonlin.m
+++ nonlin.m
@@ -1,15 +1,15 @@
 function dis=nonlin(a)
 N=max(size(a));
 n=log2(N);
-%Создание аффинного кода
-%Формирование линейного когда
+% Generate Affine Code
+% Linear code generation
 for i=1:N
 lin(i,1:n)=bitget(i-1, 1:n);
 end
 affin=mod(lin*lin',2);
-%Формирование аффинного кода    
+%Formation of an affine code 
 affin1=[affin; mod(affin+1,2); a]; 
-%Нахождения расстояний Хэмминга
+% Finding Hamming Distances
 h1=2*N+1;
 for h=1:2*N
     if h1 ~=h
